3ca302005da9ade206a36d81b106cd7638b6002b
logicSt <olumide.f.akinduko@gmail.com>
2022-03-11 08:21:05 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * program that prints the alphabet in lowercase,
+ * followed by a new line
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+char alphabet;
+
+alphabet = 'a';
+
+while (alphabet <= 'z')
+{
+if (alphabet == 'e' || alphabet == 'q')
+continue;
+
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+
+return (0);
+}
